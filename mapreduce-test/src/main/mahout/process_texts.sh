09185b18bf6c58ec533faafc49178df347e29004
tharter <tod.g.harter@boeing.com>
2017-05-11 16:26:45 -0400

    Working mahout demo

--- mapreduce-test/src/main/mahout/process_texts.sh
+++ mapreduce-test/src/main/mahout/process_texts.sh
@@ -3,5 +3,5 @@ hadoop fs -mkdir mahout
 hadoop fs -copyFromLocal src/main/mahout/texts mahout/texts
 $MAHOUT_HOME/bin/mahout seqdirectory --input mahout/texts --output mahout/texts-seq --overwrite
 $MAHOUT_HOME/bin/mahout seq2sparse --input mahout/texts-seq --output mahout/texts-sparse --overwrite
-$MAHOUT_HOME/bin/mahout trainnb --input mahout/texts-sparse/tfidf-vectors --output mahout/texts-train --overwrite 
-$MAHOUT_HOME/bin/mahout clusterdump --input mahout/texts-train --outputFormat CSV --numWords 10
+$MAHOUT_HOME/bin/mahout kmeans -i mahout/texts-sparse/tfidf-vectors -c mahout/canopy -o mahout/kmeans -ow -k 3 --maxIter 10
+$MAHOUT_HOME/bin/mahout clusterdump --input mahout/kmeans --outputFormat CSV --numWords 10
